41f93b346579c1d6f50291cb0c96214158458b2c
kyle- <kyle-smith1@outlook.com>
2023-11-30 17:36:18 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+// Classes 
+
+public class InventoryItem
+{
+    public double Weight { get; set; }
+    public double Volume { get; set; }
+
+    public InventoryItem(double weight, double volume)
+    {
+        Weight = weight;
+        Volume = volume;
+    }
+
+}
+
+public class Arrow : InventoryItem
+{
+    public Arrow(double weight, double volume) : base(weight, volume)
+    {
+    }
+}
