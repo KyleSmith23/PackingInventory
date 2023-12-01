5e40a60061ee477c539f7afdf129b270f7d47b91
kyle- <kyle-smith1@outlook.com>
2023-12-01 18:47:10 +0000

    All inventory item classes created

--- Program.cs
+++ Program.cs
@@ -20,7 +20,38 @@ public class InventoryItem
 
 public class Arrow : InventoryItem
 {
-    public Arrow(double weight, double volume) : base(weight, volume)
+    public Arrow() : base(0.1, 0.05)
     {
     }
 }
+public class Bow : InventoryItem
+{
+    public Bow() : base(1, 4)
+    {
+    }
+}
+public class Rope : InventoryItem
+{
+    public Rope() : base(1, 1.5)
+    {
+    }
+}
+public class Water : InventoryItem
+{
+    public Water() : base(2, 3)
+    {
+    }
+}
+public class Food : InventoryItem
+{
+    public Food() : base(1, 0.5)
+    {
+    }
+}
+public class Sword : InventoryItem
+{
+    public Sword() : base(5, 3)
+    {
+    }
+}
+
